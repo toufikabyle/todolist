b6bc4453de3cf59425dd10a9fbdfc1e516d08f7d
toufik bouaoun <bouaoun.toufik@gmail.com>
2021-03-13 13:55:36 -0500

    Updates

---
+++ precache-manifest.1ece4ae8b199ff06b4dce7a8fef914eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4e75c80a5ecf69f933",
+    "url": "/todolist/static/css/main.55811c7c.chunk.css"
+  },
+  {
+    "revision": "da4e75c80a5ecf69f933",
+    "url": "/todolist/static/js/main.da4e75c8.chunk.js"
+  },
+  {
+    "revision": "5606f2708e54d37f4001",
+    "url": "/todolist/static/js/1.5606f270.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "52a803671fb9e5bc330005dc8e179300",
+    "url": "/todolist/index.html"
+  }
+];
